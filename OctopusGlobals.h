a4078de4ce8f0d97fe7c24f159dce1033dc3e33a
jliphard <jan.liphardt@stanford.edu>
2015-02-10 16:22:55 -0800

    Update OctopusGlobals.h

--- OctopusGlobals.h
+++ OctopusGlobals.h
@@ -17,36 +17,17 @@ struct COctopusROI
 	int bin;
 };
 
-struct COctopusCentroid
-{
-	double X;
-	double Y;
-	double Intensity;
-	double max;
-	double min;
-	double mean;
-	double totalint;
-	double cut_high;
-
-	double LM_X;
-	double LM_Y;
-	double LM_X_sd;
-	double LM_Y_sd;
-	double LM_fwhm;
-
-};
-
 struct COctopusGlobals 
 {
-	u32		files_written;
+	u32	files_written;
 	bool	savetofile;
-	u16		W;
-	u16		H;
+	u16	W;
+	u16	H;
 	bool	automatic_gain;
 	double	manual_gain;
-	u32		pics_per_file;
-	u16		CCD_x_phys_e;
-	u16		CCD_y_phys_e;
+	u32	pics_per_file;
+	u16	CCD_x_phys_e;
+	u16	CCD_y_phys_e;
 	bool	Camera_Thread_running;
 	CString pathname;
 	u16     bin;
@@ -66,18 +47,12 @@ struct COctopusGlobals
 	COctopusROI ROI_target;
 	COctopusROI Focus_ROI;
 	bool        Focus_ROI_Set;
-	double volts;
+	double 	    volts;
 	u32         pictures_to_skip;
 
 	//let's try this again
 	bool        SetFocus;
 
-	//Shutter and filter
-	u8      nd_setting;
-	bool    load_wheel_failed;
-	u8      filter_wheel;
-	bool    program_running;
-	
 	//autofocus
 	bool    focus_in_progress;
 	double  focus_score;
@@ -91,51 +66,11 @@ struct COctopusGlobals
 	double	focus_min;
 	double	focus_max;
 
-	//piezo and scope
-	double  position_x;
-	double  position_y;
-	double  position_z;
-
-	double  position_x_volt;
-	double  position_y_volt;
-	double  position_z_volt;
-
-	double ADC_1;
-
-	double ADC_20Hz;
-	double ADC_1Hz;
-	double ADC_01Hz;
-
-	bool    LED_loaded;
-	bool    LED_was_on;
-	bool    LED_is_on;
-	u16     LED_intensity_target;
-	u16     LED_intensity_current;
-
-	bool Laser_561_is_On;
-	bool Laser_488_is_On;
-	bool Laser_405_is_On;
-	bool Laser_639_is_On;
-	bool Lasers_loaded;
-
-	bool Cleaning;
-
-	//AOTF
-	bool AOTF_loaded;
-	bool AOTF_running;
-
 	//MPC200
 	bool MPC200_loaded;
 	double MPC_m_nXCurr_Micron;
 	double MPC_m_nYCurr_Micron;
 	double MPC_m_nZCurr_Micron;
-
-	//scope
-	bool Scope_loaded;
-	double position_z_objective;
-
 };
-
-
 
 #endif
