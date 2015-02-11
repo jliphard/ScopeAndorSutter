c56f5c2e6e6a3e1b8dba8f717d30fbf5f532d5bd
jliphard <jan.liphardt@stanford.edu>
2015-02-10 16:06:16 -0800

    Update OctopusView.h

--- OctopusView.h
+++ OctopusView.h
@@ -23,21 +23,13 @@ public:
 protected:
 
 	afx_msg void OnOpenAndor();
-	afx_msg void OnOpenMotor();
 	afx_msg void OnOpenMPC200();
-
 	CTime systemtime;
-
 	virtual void OnInitialUpdate();
 	virtual void DoDataExchange(CDataExchange* pDX);
-
 	u16 scope;
-
 	DECLARE_MESSAGE_MAP()
-
 	CString debug;
-
-	
 };
 
 #endif
