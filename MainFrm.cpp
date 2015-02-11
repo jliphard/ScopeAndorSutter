75031b9398dc281667cbf15ca200b0aa494ff7fd
jliphard <jan.liphardt@stanford.edu>
2015-02-10 16:12:36 -0800

    Update MainFrm.cpp

--- MainFrm.cpp
+++ MainFrm.cpp
@@ -11,10 +11,10 @@ CMainFrame::~CMainFrame() {}
 BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs) 
 {
 	cs.style = WS_VISIBLE | WS_SYSMENU | WS_OVERLAPPEDWINDOW;
-    cs.cy = 950; 
-    cs.cx = 928; 
+    	cs.cy = 950; 
+    	cs.cx = 928; 
 	cs.y  = 2; 
-    cs.x  = 2;
+    	cs.x  = 2;
 
 	return CFrameWnd::PreCreateWindow(cs);
 }
